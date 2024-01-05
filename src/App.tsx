15e6c9c3c424a9edb41488af9a229ac6de1eaba7
Jaspreet Singh <erjaspreet57@gmail.com>
2024-01-06 00:00:45 +0530

    Setup: add MUI - Material UI library to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@mui/material/Button";
+
+function ButtonUsage() {
+  return (
+    <Button variant="contained" style={{ marginTop: "20px" }}>
+      Hello world
+    </Button>
+  );
+}
 
 function App() {
   return (
@@ -18,6 +27,8 @@ function App() {
         >
           Learn React
         </a>
+
+        <ButtonUsage />
       </header>
     </div>
   );
